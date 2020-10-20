49e52eeaa3f1c11ec46005213c5565cbb4cfc243
Thomas Wouters <thomas@python.org>
2020-10-20 14:23:19 +0200

    Extend test cases.

--- Lib/test/test_unused_variable.py
+++ Lib/test/test_unused_variable.py
@@ -6,36 +6,76 @@ class UnusedAssignmentTest(unittest.TestCase):
         ? = 1
         x, ?, z = range(3)
         self.assertEqual((x, z), (0, 2))
+        x, ?, ?, z, ? = range(5)
+        self.assertEqual((x, z), (0, 3))
+        ?, x, ?, z, ? = range(5)
+        self.assertEqual((x, z), (1, 3))
+
+    def test_splat(self):
         x, *?, z = range(10)
         self.assertEqual((x, z), (0, 9))
+        x, ?, ?, *?, z, ? = range(10)
+        self.assertEqual((x, z), (0, 8))
+        ?, x, *?, z = range(10)
+        self.assertEqual((x, z), (1, 9))
 
     def test_for_loop(self):
         i = 0
         for ? in range(3):
             i += 1
         self.assertEqual(i, 3)
+        i = 0
+        for ?, ? in enumerate("abc"):
+            i += 1
+        self.assertEqual(i, 3)
+        i = 0
+        for idx, ? in enumerate("abc"):
+            self.assertEqual(idx, i)
+            i += 1
     
     def test_comprehension(self):
         l = [ 1 for ? in range(3) ]
         self.assertEqual(l, [1, 1, 1])
+        l = [ 1 for ?, *? in enumerate(range(3)) ]
+        self.assertEqual(l, [1, 1, 1])
+        with self.assertRaises(TypeError):
+            l = [ 1 for ?, ? in range(3) ]
+        l = [ 1 for ?, ? in enumerate("abc") ]
+        self.assertEqual(l, [1, 1, 1])
+        l = [ idx for idx, ? in enumerate("abc") ]
+        self.assertEqual(l, [0, 1, 2])
         l = list(1 for ? in range(3))
         self.assertEqual(l, [1, 1, 1])
+        l = list(1 for ?, ? in enumerate("abc"))
+        self.assertEqual(l, [1, 1, 1])
+        l = list(idx for idx, ? in enumerate("abc"))
+        self.assertEqual(l, [0, 1, 2])
         s = {1 for ? in range(3) }
         self.assertEqual(s, {1})
-        d = {'k': 'v' for ? in range(3) }
+        s = {1 for ?, ? in enumerate("abc")}
+        self.assertEqual(s, {1})
+        s = {idx for ?, idx in enumerate("abc")}
+        self.assertEqual(s, {"a", "b", "c"})
+        d = {'k': 'v' for ? in range(3)}
         self.assertEqual(d, {'k': 'v'})
+        d = {'k': 'v' for ?, ? in enumerate("abc")}
+        self.assertEqual(d, {'k': 'v'})
+        d = {k: None for k, ? in enumerate("abc")}
+        self.assertEqual(d, {0: None, 1: None, 2: None})
 
     def test_with(self):
+        called = False
         @contextlib.contextmanager
         def manager(n):
+            nonlocal called
+            called = True
             yield range(n)
         with manager(10) as ?:
-            pass
+            self.assertTrue(called)
         with manager(3) as (x, ?, z):
             self.assertEqual((x, z), (0, 2))
-        # TODO(twouters): figure out why this doesn't work.
-        # with manager(10) as (x, *?, z):
-        #     self.assertEqual((x, z), (0, 9))
+        with manager(5) as (?, x, ?, z, ?):
+            self.assertEqual((x, z), (1, 3))
 
 if __name__ == '__main__':
     unittest.main()
